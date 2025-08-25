5b95cc0df731ea6ccd853d2b456e34e9ad961401
kshittiz101 <chaudharykshittiz950@gmail.com>
2025-08-25 18:45:35 +0545

    Apply black/isort/ruff fixes

--- notes/models.py
+++ notes/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.conf import settings
+from django.db import models
 
 
 class Note(models.Model):
@@ -14,8 +14,7 @@ class Note(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['-updated_at']
+        ordering = ["-updated_at"]
 
     def __str__(self):
         return self.title[:50]
-
